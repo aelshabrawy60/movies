a8aff3396ea9cdc66187eefcda54d0154a4a5427
ahmed <aelshabrawy60@gmail.com>
2025-04-26 00:45:38 +0300

    io

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <>
         {/* Navigation */}
-        <nav className="px-6 py-4 backdrop-blur-md bg-black/30 sticky top-0 z-10">
+        <nav className="px-6 py-4 bg-black/50 sticky top-0 z-10">
             <div className="container mx-auto flex justify-between items-center">
                 <Link href={'/'}>
                   <img src='/ambient-light-logo.svg'></img>
